572fed6dfbe07e99921f88fd884583796b484bcd
Ashu-panchal <preset2408@gmail.com>
2025-08-20 14:04:42 +0530

    initial commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 3000
 app.use(express.json())
 app.use(cors({
-  origin: "http://localhost:3001"
+  origin: "https://todo-app-frontend-4rxw.onrender.com"
 }));
 
 let db;
